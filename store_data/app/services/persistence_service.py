6164df4d67e1f1f9374d82bf0a993079cdf4a698
salmaneyanis <yanissalmane@gmail.com>
2026-05-20 11:35:16 +0200

    les fonctions get, update, delete, list et create de persistence_service

---
+++ store_data/app/services/persistence_service.py
@@ -0,0 +1,57 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.document_model import Document
+from app.schemas.document_schemas import DocumentCreate, DocumentUpdate
+
+
+async def create_document(db: AsyncSession, data: DocumentCreate) -> Document:
+    """  """
+    doc = Document(
+        file_name=data.file_name,
+        category=data.category,
+    )
+
+    db.add(doc)
+
+    await db.commit()
+    await db.refresh(doc)
+    return doc
+
+
+
+async def get_document(db: AsyncSession, doc_id: int) -> Document | None:
+    result = await db.execute(select(Document).where(Document.id == doc_id))
+    document = result.scalar_one_or_none()
+
+    return document
+
+async def list_documents(db: AsyncSession, skip: int = 0, limit: int = 100) -> list[Document]:
+    result = await db.execute(
+        select(Document)
+        .order_by(Document.created_at.desc())
+        .limit(limit)
+        .offset(skip)
+    )
+    return result.scalars().all()
+
+async def update_document(db: AsyncSession, doc_id: int, data: DocumentUpdate) -> Document:
+    doc = await get_document(db,doc_id)
+
+    #extrait les colonnes données (on exclue les colonnes vides)
+    update_data = data.model_dump(exclude_unset=True)
+    #on insère les fields dans le doc afin de mettre à jour les données
+    for field, value in update_data.items():
+        setattr(doc,field,value)
+    
+    await db.commit()
+    await db.refresh(doc)
+    return doc
+    
+
+async def delete_document(db: AsyncSession, doc_id: int) -> None:
+    result = await db.execute(select(Item).filter(Item.id == item_id))
+    db_item = result.scalar_one_or_none()
+    await db.delete(d_item)
+    await db.commit()
+    return None
