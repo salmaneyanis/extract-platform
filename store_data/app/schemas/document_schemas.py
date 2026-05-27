c007a293c22d9b4afa3555f51f22cba0ee46ad1f
salmaneyanis <yanissalmane@gmail.com>
2026-05-27 10:29:19 +0200

    Ajout des schémas pour les requêtes  Job et DocumentParse

--- store_data/app/schemas/document_schemas.py
+++ store_data/app/schemas/document_schemas.py
@@ -40,3 +40,36 @@ class DocumentResponse(BaseModel):
     
     model_config = ConfigDict(from_attributes=True)
 
+class DocumentParseCreate(BaseModel):
+    doc_id: int
+
+class DocumentParseResponse(BaseModel):
+    parse_id: int
+    doc_id: int 
+    representation_type: str | None 
+    content_json: dict | None
+    content_text: str | None
+    created_at: datetime
+
+
+
+class JobCreate(BaseModel):
+    doc_id: int 
+    job_type: str
+
+class JobUpdate(BaseModel): 
+    file_path: str | None = None
+    job_type: str | None = None
+
+
+class JobResponse(BaseModel):
+    job_id: int
+    job_type: str
+    status: Status
+    result: dict
+    error: str
+    created_at: datetime
+    started_at: datetime
+    finished_at: datetime
+
+
